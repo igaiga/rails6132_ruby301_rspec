c6c718f0b155358ea0126f4badffb86da5b3fcb9
Kuniaki Igarashi <igaiga@gmail.com>
2021-05-19 16:49:36 +0900

    Writing Book#title_with_author spec with context

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,10 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Book, type: :model do
-  describe "Book#title_with_author" do # describeメソッドをつかってメソッドごとに区切ると読みやすいです
-    it "Book#title_with_authorを呼び出したとき、titleとauthorを結んだ文字列が返ること" do
-      book = Book.new(title: "RubyBook", author: "matz")
-      expect(book.title_with_author).to eq("RubyBook - matz")
+  describe "Book#title_with_author" do
+    context "Book#titleが文字列のとき" do
+      it "titleとauthorを結んだ文字列が返ること" do
+        book = Book.new(title: "RubyBook", author: "matz")
+        expect(book.title_with_author).to eq("RubyBook - matz")
+      end
+    end
+    context "Book#titleがnilのとき" do
+      it "空のtitleとauthorを結んだ文字列が返ること" do
+        book = Book.new(author: "matz")
+        expect(book.title_with_author).to eq(" - matz")
+      end
     end
   end
 end
