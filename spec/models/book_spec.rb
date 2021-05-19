7960b0d9fc93483569ae939ff08b884dce99adec
Kuniaki Igarashi <igaiga@gmail.com>
2021-05-19 17:06:21 +0900

    Use subject in Book model spec

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -3,15 +3,15 @@ require 'rails_helper'
 RSpec.describe Book, type: :model do
   describe "Book#title_with_author" do
     context "Book#titleが文字列のとき" do
+      subject { Book.new(title: "RubyBook", author: "matz") }
       it "titleとauthorを結んだ文字列が返ること" do
-        book = Book.new(title: "RubyBook", author: "matz")
-        expect(book.title_with_author).to eq("RubyBook - matz")
+        expect(subject.title_with_author).to eq("RubyBook - matz")
       end
     end
     context "Book#titleがnilのとき" do
+      subject(:book){ Book.new(author: "matz") } # subjectに名前をつけることもできる
       it "空のtitleとauthorを結んだ文字列が返ること" do
-        book = Book.new(author: "matz")
-        expect(book.title_with_author).to eq(" - matz")
+        expect(book.title_with_author).to eq(" - matz") # bookはsubject(:book)を指す
       end
     end
   end
