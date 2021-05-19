42a9e316113a5644c8d328381a830a7ed0519e9a
Kuniaki Igarashi <igaiga@gmail.com>
2021-05-19 17:28:51 +0900

    Use let,let! in Book model spec

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -3,17 +3,15 @@ require 'rails_helper'
 RSpec.describe Book, type: :model do
   describe "Book#title_with_author" do
     context "Book#titleが文字列のとき" do
-      before do
-        @book = Book.new(title: "RubyBook", author: "matz")
-      end
+      let(:book){ Book.new(title: "RubyBook", author: "matz") }
       it "titleとauthorを結んだ文字列が返ること" do
-        expect(@book.title_with_author).to eq("RubyBook - matz")
+        expect(book.title_with_author).to eq("RubyBook - matz")
       end
     end
     context "Book#titleがnilのとき" do
-      subject(:book){ Book.new(author: "matz") } # subjectに名前をつけることもできる
+      let!(:book){ Book.new(author: "matz") }
       it "空のtitleとauthorを結んだ文字列が返ること" do
-        expect(book.title_with_author).to eq(" - matz") # bookはsubject(:book)を指す
+        expect(book.title_with_author).to eq(" - matz")
       end
     end
   end
