f70cf5983d2be40c6b44181700adbb13809ae2b2
Kuniaki Igarashi <igaiga@gmail.com>
2021-05-19 18:21:17 +0900

    Writing Book factory

--- spec/factories/books.rb
+++ spec/factories/books.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
-  factory :book do
-    title { "MyString" }
-    author { "MyString" }
+  factory :book do # :book はモデル名を全小文字にしてシンボルにしたもの
+    title { "RubyBook" } # カラム title のデータとして "RubyBook" をつかう
+    author { "matz" } # カラム author のデータとして "matz" をつかう
   end
 end
