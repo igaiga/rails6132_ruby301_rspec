53ba4e28463e1cf4085d8b473cb64979c5ca3dcb
Kuniaki Igarashi <igaiga@gmail.com>
2021-05-19 16:39:13 +0900

    Writing Book#title_with_author spec with describe

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,5 @@
 class Book < ApplicationRecord
+  def title_with_author
+    "#{title} - #{author}"
+  end
 end
--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,7 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Book, type: :model do
-  it "Bookモデルをnewしたとき、nilではないこと" do
-    expect(Book.new).not_to eq(nil)
+  describe "Book#title_with_author" do # describeメソッドをつかってメソッドごとに区切ると読みやすいです
+    it "Book#title_with_authorを呼び出したとき、titleとauthorを結んだ文字列が返ること" do
+      book = Book.new(title: "RubyBook", author: "matz")
+      expect(book.title_with_author).to eq("RubyBook - matz")
+    end
   end
 end
