66300bb0f331bc99faa58bde7e8821f71f60d547
Kuniaki Igarashi <igaiga@gmail.com>
2021-05-19 17:26:37 +0900

    Use before in Book model spec

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -3,9 +3,11 @@ require 'rails_helper'
 RSpec.describe Book, type: :model do
   describe "Book#title_with_author" do
     context "Book#titleが文字列のとき" do
-      subject { Book.new(title: "RubyBook", author: "matz") }
+      before do
+        @book = Book.new(title: "RubyBook", author: "matz")
+      end
       it "titleとauthorを結んだ文字列が返ること" do
-        expect(subject.title_with_author).to eq("RubyBook - matz")
+        expect(@book.title_with_author).to eq("RubyBook - matz")
       end
     end
     context "Book#titleがnilのとき" do
