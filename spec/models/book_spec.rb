f407a7058c572539f14a033c8eeb6db29d2f6ff3
Kuniaki Igarashi <igaiga@gmail.com>
2021-05-19 16:00:34 +0900

    Writing Book.new spec

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Book, type: :model do
-  it "trueであるとき、trueになること" do
-    expect(true).to eq(true)
+  it "Bookモデルをnewしたとき、nilではないこと" do
+    expect(Book.new).not_to eq(nil)
   end
 end
