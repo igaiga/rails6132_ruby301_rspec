c17cb3aa9727848fa3d3955fe5a3730f74f3f734
Kuniaki Igarashi <igaiga@gmail.com>
2021-05-19 15:22:36 +0900

    Writing a NG model spec code

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Book, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "trueであるとき、falseになること" do
+    expect(true).to eq(false)
+  end
 end
