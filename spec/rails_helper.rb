f4df6cd84cfffb6e6ab3a6ac50af9cc7c8219bda
Kuniaki Igarashi <igaiga@gmail.com>
2021-05-19 18:52:04 +0900

    Setup for system spec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -61,4 +61,10 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  config.before(:each, type: :system) do
+    driven_by(:selenium_chrome_headless)
+    # driven_by(:selenium_chrome)
+    # driven_by(:rack_test)
+  end
 end
