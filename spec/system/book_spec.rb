83068c5cec4dcca2c6e960f4d534a9913bc099b3
Kuniaki Igarashi <igaiga@gmail.com>
2021-05-19 19:09:17 +0900

    Writing POST System spec

--- spec/system/book_spec.rb
+++ spec/system/book_spec.rb
@@ -1,8 +1,19 @@
 require "rails_helper"
 
 RSpec.describe "books", type: :system do
-  it "/books ページが表示されること" do
-    visit "/books" # /booksへHTTPメソッドGETでアクセス
-    expect(page).to have_text("Books") # 表示されたページに Books という文字があることを確認
+  it "GET /books" do
+    visit "/books"
+    expect(page).to have_text("Books")
+  end
+
+  it "POST /books" do
+    visit "/books/new"
+    fill_in "Title", with: "RubyBook" # Title入力欄に"RubyBook"を入力する
+    fill_in "Author", with: "matz"
+    click_button "Create Book" # Create Bookボタンを押す
+
+    expect(page).to have_text("Book was successfully created.")
+    expect(page).to have_text("Title: RubyBook")
+    expect(page).to have_text("Author: matz")
   end
 end
