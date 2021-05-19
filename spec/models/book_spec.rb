a35f4fa852fe64550a1923dd8650e020cd7b397d
Kuniaki Igarashi <igaiga@gmail.com>
2021-05-19 15:54:35 +0900

    Writing a OK model spec code

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Book, type: :model do
-  it "trueであるとき、falseになること" do
-    expect(true).to eq(false)
+  it "trueであるとき、trueになること" do
+    expect(true).to eq(true)
   end
 end
